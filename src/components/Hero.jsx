f34a5bfb9207285e4ac04287b0857247e710ba53
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-07-05 10:02:18 +0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import hero1 from '../assets/img/hero/hero-1.jpg'
+import hero2 from '../assets/img/hero/hero-2.jpg'
+
+const Hero = () => {
+  return (
+    <div>
+   
+ 
+    </div>
+  )
+}
+
+export default Hero
